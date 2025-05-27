9c325c8891c878473eaf8a237d319cd4b5538e62
John Limpiada <john.limpiada@felice.ed.jp>
2025-05-27 03:44:50 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Console Error: [next-international] The locale 'en' is not supported. Defined locales are: [locales, defaultLocale, localeCookie].. Error source: src/app/layout.tsx (50:9) @ RootLayout
    >
    >   48 |       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
    >   49 |         {/* Use the resolved currentLocale for the I18nProviderClient */}
    > > 50 |         <I18nProviderClient locale={currentLocale}>
    >      |         ^
    >   51 |           <AuthProvider>
    >   52 |             {children}
    >   53 |             <Toaster />
    >
    > Call Stack
    > 7
    >
    > Show 6 ignore-listed frame(s)
    > RootLayout
    > src/app/layout.tsx (50:9)

--- src/lib/i18n/client.ts
+++ src/lib/i18n/client.ts
@@ -1,8 +1,13 @@
 
 'use client';
 import { createI18nClient } from 'next-international/client';
-import { getLocaleConfig } from './settings';
+// We don't need getLocaleConfig here for createI18nClient
+// import { getLocaleConfig } from './settings'; 
 
 export const { useI18n, useScopedI18n, I18nProviderClient, useChangeLocale, useCurrentLocale } = createI18nClient(
-  getLocaleConfig()
+  {
+    en: () => import('@/locales/en'),
+    ja: () => import('@/locales/ja'),
+  }
 );
+
--- src/lib/i18n/server.ts
+++ src/lib/i18n/server.ts
@@ -1,7 +1,12 @@
 
 import { createI18nServer } from 'next-international/server';
-import { getLocaleConfig } from './settings';
+// We don't need getLocaleConfig here for createI18nServer
+// import { getLocaleConfig } from './settings';
  
 export const { getI18n, getScopedI18n, getStaticParams, getCurrentLocale } = createI18nServer(
-  getLocaleConfig()
+  {
+    en: () => import('@/locales/en'),
+    ja: () => import('@/locales/ja'),
+  }
 );
+
